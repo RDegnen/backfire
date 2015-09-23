332265c0041c5070a490ba4dfee182929a2d3c04
Ross <rdegnen@gmail.com>
2015-09-23 11:05:02 -0400

    finished initial setup

--- app/scripts/views/appView.js
+++ app/scripts/views/appView.js
@@ -2,9 +2,13 @@
 
 var AppView = Backbone.View.extend({
   el: $('#backfire'),
+  events: {
+    "click add-todo": "createTodo",
+  },
 
   initialize: function() {
     this.list = this.$("#todo-list");
+    this.input = this.$("#new-todo");
 
     this.listenTo(this.collection, 'add', this.addOne);
   },
@@ -12,5 +16,13 @@ var AppView = Backbone.View.extend({
   addOne: function(todo) {
     var view = new TodoView({model: todo});
     this.list.append(view.render().el);
+  },
+
+  createTodo: function(e) {
+    if (!this.input.val()) { return; }
+
+    this.collection.create({title: this.input.val()});
+
+    this.input.val('');
   }
 });
