fbd3b47ad516c2c227f45db02c86f6e879eebb25
Ross <rdegnen@gmail.com>
2015-09-23 10:42:30 -0400

    added todo  view

---
+++ app/scripts/views/todoView.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var TodoView = Backbone.View.extend({
+  tagName: "li",
+  template: _.template("<%= title %>"),
+
+  initialize: function() {
+    this.listenTo(this.model, "change", this.render);
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+    return this;
+  }
+
+});
