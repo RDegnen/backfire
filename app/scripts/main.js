1552290e50d70419a4b4ae00f3e58ec2c5467faf
Ross <rdegnen@gmail.com>
2015-09-28 15:21:04 -0400

    solved it, variables were being declared on doc ready. changed to being declared in function call

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,6 +5,22 @@ function init() {
   var app = new AppView({ collection: collection});
 }
 
+function showBtn () {
+  var hasDone = $('ul').find('li').hasClass('done');
+
+  if (hasDone) {
+    $('#remove').show();
+  } else {
+    $('#remove').hide();
+  }
+}
+
 $(document).ready(function() {
   init();
+
+  $('#remove').hide();
+
+  $('ul').on('click', function() {
+    showBtn();
+  });
 });
